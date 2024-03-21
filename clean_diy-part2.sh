9700d59faa81fcc748a6a7426f49b91f3c78ffc0
Axiu0823 <69513816+Axiu0823@users.noreply.github.com>
2024-03-21 13:30:48 +0800

    Update clean_diy-part2.sh

--- clean_diy-part2.sh
+++ clean_diy-part2.sh
@@ -10,14 +10,24 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
+# 修改默认IP192.168.1.1为后边那个ip
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
+# 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+ 
+# 修改默认主机名
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Router'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+#sed -i "s/OpenWrt /Kinoko build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+ 
+
+
 
 # drop mosdns and v2ray-geodata packages that come with the source 删除源代码附带的 modns 和 v2ray-Geodata 包
 #find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
