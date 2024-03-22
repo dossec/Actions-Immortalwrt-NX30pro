e00d14149a88e1e03b44361f87ff34093d904ec4
Axiu0823 <69513816+Axiu0823@users.noreply.github.com>
2024-03-22 08:46:52 +0800

    Update science_diy-part1.sh

--- science_diy-part1.sh
+++ science_diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
